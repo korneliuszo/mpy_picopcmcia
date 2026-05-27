eb7f44dfe4a33804e27aa7c1a8a2915ae0c82862
Korneliusz Osmenda <korneliuszo@gmail.com>
2026-05-27 22:03:32 +0200

    Common rom class (untested)

--- src/cmodules/picopcmcia_low/micropython.cmake
+++ src/cmodules/picopcmcia_low/micropython.cmake
@@ -5,11 +5,13 @@ add_library(picopcmcia_low INTERFACE)
 target_sources(picopcmcia_low INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/picopcmcia_low.c
     ${CMAKE_CURRENT_LIST_DIR}/attr_rom.c
+    ${CMAKE_CURRENT_LIST_DIR}/common_rom.c
 )
 
 set_source_files_properties(
     ${CMAKE_CURRENT_LIST_DIR}/picopcmcia_low.c
     ${CMAKE_CURRENT_LIST_DIR}/attr_rom.c
+    ${CMAKE_CURRENT_LIST_DIR}/common_rom.c
     PROPERTIES COMPILE_FLAGS "-O3"
 )
 
