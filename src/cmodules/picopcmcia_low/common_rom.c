5e79cfde9bf0e463c254aaaaf431f06166757634
Korneliusz Osmenda <korneliuszo@gmail.com>
2026-06-04 21:57:24 +0200

    ramrom

--- src/cmodules/picopcmcia_low/common_rom.c
+++ src/cmodules/picopcmcia_low/common_rom.c
@@ -1,6 +1,7 @@
 #include "py/runtime.h"
 #include "pico.h"
 #include "picopcmcia_proto.h"
+#include "hardware/gpio.h"
 #include "stdint.h"
 
 // This structure represents Timer instance objects.
@@ -41,6 +42,25 @@ static uint32_t __no_inline_not_in_flash_func(common_rom_fn)(void * self,uint32_
     if(!(mux1 & (1<<23))) //CE1 low
         data |= (buf[addr+0]<<0) | (0x00ff0000ul);
     return data;
+}
+
+static uint32_t __no_inline_not_in_flash_func(common_ram_fn)(void * self,uint32_t mux0,uint32_t mux1,uint32_t idx)
+{
+    common_rom_ROM_obj_t * self_ptr = self;
+    uint32_t addr = mux0 - 0x4000;
+    if(!(mux1 & (1<<24))) //CE2 low
+        addr&=~0x1ul;
+    if(self_ptr->bufinfo.len < addr)
+        return 0;
+    uint8_t * buf;
+    if(!(buf = self_ptr->bufinfo.buf))
+        return 0;
+    uint16_t data = gpio_get_all()&0xffff;//should be function
+    if(!(mux1 & (1<<24))) //CE2 low
+        buf[addr+1] = data >> 8;
+    if(!(mux1 & (1<<23))) //CE1 low
+        buf[addr+0] = data >> 0;
+    return 0;
 }
 
 // This represents Timer.__new__ and Timer.__init__, which is called when
@@ -93,6 +113,47 @@ MP_DEFINE_CONST_OBJ_TYPE(
     locals_dict, &common_rom_ROM_locals_dict
     );
 
+    // This represents Timer.__new__ and Timer.__init__, which is called when
+// the user instantiates a Timer object.
+static mp_obj_t common_rom_RAM_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {
+    // Allocates the new object and sets the type.
+    common_rom_ROM_obj_t *self = mp_obj_malloc(common_rom_ROM_obj_t, type);
+
+    enum { ARG_buff };
+    static const mp_arg_t allowed_args[] = {
+        { MP_QSTR_buff, MP_ARG_OBJ, {.u_rom_obj = MP_ROM_NONE} },
+    };
+
+    // Parse the arguments.
+    mp_arg_val_t args_out[MP_ARRAY_SIZE(allowed_args)];
+    mp_arg_parse_all_kw_array(n_args, n_kw, args, MP_ARRAY_SIZE(allowed_args), allowed_args, args_out);
+
+    self->bufinfo.buf = NULL;
+    self->bufinfo.len = 0;
+    self->bufinfo.typecode = (int)'b';
+    self->base.fn = common_ram_fn;
+
+    if (args_out[ARG_buff].u_obj == mp_const_none)
+    {
+        self->buff = mp_const_none;
+    }
+    else
+    {
+        self->buff = args_out[ARG_buff].u_obj;
+        mp_get_buffer_raise(self->buff,&self->bufinfo,MP_BUFFER_READ);
+    }
+    // The make_new function always returns self.
+    return MP_OBJ_FROM_PTR(self);
+}
+
+MP_DEFINE_CONST_OBJ_TYPE(
+    common_rom_type_RAM,
+    MP_QSTR_RAM,
+    MP_TYPE_FLAG_NONE,
+    make_new, common_rom_RAM_make_new,
+    locals_dict, &common_rom_ROM_locals_dict
+    );
+
 // Define all properties of the module.
 // Table entries are key/value pairs of the attribute name (a string)
 // and the MicroPython object reference.
@@ -101,6 +162,7 @@ MP_DEFINE_CONST_OBJ_TYPE(
 static const mp_rom_map_elem_t common_rom_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_common_rom_low) },    
     { MP_ROM_QSTR(MP_QSTR_rom), MP_ROM_PTR(&common_rom_type_ROM) },
+    { MP_ROM_QSTR(MP_QSTR_ram), MP_ROM_PTR(&common_rom_type_RAM) },
 };
 
 static MP_DEFINE_CONST_DICT(common_rom_module_globals, common_rom_module_globals_table);
